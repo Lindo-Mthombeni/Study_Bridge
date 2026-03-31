71fd7c588103dd24ad0a799ddf5b9e7202ce05e4
Lindo-Mthombeni <ntsakomthombeni13@gmail.com>
2026-03-31 11:36:10 +0200

    Update Code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -60,7 +60,7 @@ export const Hero: React.FC = () => {
         <div className='text-main text-content pt-0 font-semibold'>
           Access study notes, practice quizzes, and past exam papers aligned
           with the South African curriculum. Bridging students to success, for
-          free.{delay}true
+          free.{delay}false
         </div>
         <div className='call-to-action'>
           <Button
